89e99466ce7c42b7c1e3d7bf9ad50892c7627ff0
EnanderW <williamenander@gmail.com>
2025-02-07 13:52:51 +0100

    add a few comments for basket code

--- js/components/basket.js
+++ js/components/basket.js
@@ -9,24 +9,31 @@ function setupBasket() {
     const basketOpenButton = document.getElementById("basket-open-button");
     const basketCloseButton = document.getElementById("basket-close-button");
 
+    // Öppna eller stäng basket vid knapptryck
     basketOpenButton.addEventListener("click", toggleBasket);
     basketCloseButton.addEventListener("click", toggleBasket);
 
+    // Stäng basket om man trycker utanför och den är öppen
     document.body.addEventListener("click", () => {
         if (basketWrapper.classList.contains("basket-visible")) {
             basketWrapper.classList.remove("basket-visible");
         }
     });
 
+    // Förhindra att basket stängs om man trycker innanför basket boxen
     basketWrapper.addEventListener("click", event => event.stopPropagation());
 
+    // Rensa alla basket items
     basketClearButton.addEventListener("click", () => {
         basket.items = [];
         renderBasketItems();
     });
 }
 
+// Lägger till en produkt i basket och visar upp dem
 export function addProductToBasket(product) {
+    // Kolla först om produkten finns redan
+    //  och öka i antal istället i så fall
     const existing = basket.items.find(all => all.product.id == product.id);
     if (existing !== undefined) {
         existing.amount++;
@@ -39,13 +46,17 @@ export function addProductToBasket(product) {
     renderBasketItems();
 }
 
+// Renderar ut (visar) alla basket items i basket boxen
 function renderBasketItems() {
+    // Töm först gammal data
     basketItemsElement.innerHTML = "";
 
+    // Loopa igenom alla basket items
     for (const basketItem of basket.items) {
         const article = document.createElement("article");
         article.classList.add("basket-item");
 
+        // Element för att visa informatin om produkt
         const amountSpan = document.createElement("span");
         const modelSpan = document.createElement("span");
         const priceSpan = document.createElement("span");
@@ -62,11 +73,13 @@ function renderBasketItems() {
         decAmountButton.innerText = "-";
         removeButton.innerText = "X";
 
+        // Click listener för att öka antal produkter
         incAmountButton.addEventListener("click", () => {
             basketItem.amount++;
             renderBasketItems();
         });
 
+        // Click listener för att minska antal produkter
         decAmountButton.addEventListener("click", () => {
             basketItem.amount--;
             if (basketItem.amount <= 0) {
@@ -76,6 +89,7 @@ function renderBasketItems() {
             renderBasketItems();
         });
 
+        // Click listener för att ta bort produkt från basket
         removeButton.addEventListener("click", () => {
             basket.items = basket.items.filter(item => item.product.id !== basketItem.product.id);
             renderBasketItems();
@@ -83,12 +97,14 @@ function renderBasketItems() {
 
         article.append(amountSpan, modelSpan, priceSpan, incAmountButton, decAmountButton, removeButton);
 
+        // Lägg in basket item element på sidan
         basketItemsElement.append(article);
     }
 }
 
+// Öppna eller stäng basket
 function toggleBasket(event) {
-    event.stopPropagation();
+    event.stopPropagation(); // Förhindra att andra listeners aktiveras
     basketWrapper.classList.toggle("basket-visible");
 }
 
