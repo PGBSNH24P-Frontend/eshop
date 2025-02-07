6b96880ad749de412b0d02454988cf87b6a4a5ef
EnanderW <williamenander@gmail.com>
2025-02-07 09:10:03 +0100

    add more product info

--- js/pages/product.js
+++ js/pages/product.js
@@ -5,22 +5,46 @@ const querySplit = queryParameters.split("=");
 const productId = Number.parseInt(querySplit[1]);
 const product = products.find(all => all.id === productId);
 
+let mainImageIndex = 0;
 // ?productId=5
 
 function setupProductPage() {
     renderProductImages();
+    renderMainImage();
+    renderProductInfo();
 }
 
 function renderProductImages() {
     const images = document.getElementById("product-images");
     images.innerHTML = "";
 
-    for (const imageUrl of product.images) {
+    for (let i = 0; i < product.images.length; i++) {
+        const imageUrl = product.images[i];
         const image = document.createElement("img");
         image.src = imageUrl;
 
         images.append(image);
+
+        image.addEventListener('click', () => {
+            mainImageIndex = i;
+            renderMainImage();
+        });
     }
+}
+
+function renderMainImage() {
+    const imageElement = document.getElementById("main-image");
+    imageElement.src = product.images[mainImageIndex];
+}
+
+function renderProductInfo() {
+    const categoryElement = document.getElementById("product-category");
+    const titleElement = document.getElementById("product-title");
+    const descriptionElement = document.getElementById("product-description");
+
+    categoryElement.innerText = product.category.name;
+    titleElement.innerText = product.modelName;
+    descriptionElement.innerText = product.description;
 }
 
 setupProductPage();
